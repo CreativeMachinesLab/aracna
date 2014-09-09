ff143619bfeb6fee826d9a32c3a60a2098e39e1f
Jason Yosinski <jason@yosinski.com>
2014-09-09 13:35:16 +0000

    Added more gaits to walking script

--- RobotPi/walk.py
+++ RobotPi/walk.py
@@ -2,23 +2,25 @@
 
 import argparse
 
+from commonGaits import *
 from RobotPi import *
 
 
 
 
-def runGait(gait_name, time_scale = 1.0):
+def run_gait(gait_name, time_scale = 1.0, run_time = 10.0):
     robot = RobotPi()
-    runTime = 10.0
 
     gait_function = get_gait(gait_name)
     #scaled_gait_function = scaleTime(gait_function, 1.0 / gait_speed)
     
     robot.run(gait_function,
-              runSeconds = 10,
+              runSeconds = run_time,
               resetFirst = False,
-              interpBegin = 1.0,
-              interpEnd = 1.0,
+              #interpBegin = 2.0,
+              #interpEnd = 2.0,
+              interpBegin = 0.0,
+              interpEnd = 0.0,
               timeScale = time_scale)
 
 
@@ -26,12 +28,14 @@ def runGait(gait_name, time_scale = 1.0):
 def main():
     parser = argparse.ArgumentParser(description='Plays a gait on the Aracna robot.')
     parser.add_argument('-s', '--speed', type = float, default = 1.0,
-                        help = 'Speed (default: 1.0)')
-    parser.add_argument('gait', type = str, default = 'swagger', choices = ['swagger', 'other'], nargs='?',
+                        help = 'Speed, higher is slower (default: 1.0)')
+    parser.add_argument('-t', '--time', type = float, default = 10.0,
+                        help = 'Total length of time (default: 10.0)')
+    parser.add_argument('gait', type = str, default = 'swagger', choices = ['jumpingjacks','swagger','gaita','lubricate','gait1','gait2','swagger','sine','wave', 'star6', 'star2', 'star62'], nargs='?',
                         help = 'Which gait to play, options are: swagger. Default: swagger')
     args = parser.parse_args()
 
-    runGait(args.gait, args.speed)
+    run_gait(args.gait, args.speed, args.time)
     
     print 'done'
 

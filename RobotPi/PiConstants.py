fc05124f5ce8dfbb267e86fe1693b9476047f278
Jason Yosinski <jason@yosinski.com>
2014-09-09 13:33:26 +0000

    updated Aracna constants

--- RobotPi/PiConstants.py
+++ RobotPi/PiConstants.py
@@ -1,11 +1,17 @@
 #! /usr/bin/env python
 
-MIN_INNER = 150
-MAX_INNER = 770
-MIN_OUTER = 30
+# Aracna ranges tuned with Rob
+MIN_INNER = 50
+MAX_INNER = 600
+MIN_OUTER = 0
+MAX_OUTER = 550
+
+#MIN_OUTER = 30
+#MIN_OUTER = 0
 #MAX_OUTER = 940
 #MAX_OUTER = 800 # changing because Robot hits antenna
-MAX_OUTER = 680  # changing because screws still interfere
+#MAX_OUTER = 680  # changing because screws still interfere
+#MAX_OUTER = 550  # changing because screws still interfere
 #MIN_CENTER = 512 - 180
 #MAX_CENTER = 512 + 180
 MIN_CENTER = 512 - 120
@@ -15,7 +21,8 @@ NORM_CENTER = 512
 POS_FLAT      = [512] * 8
 POS_READY     = [770,  40] * 4
 POS_HALFSTAND = [700, 100] * 4
-POS_STAND     = [512, 150] * 4
+##POS_STAND     = [512, 150] * 4
+POS_STAND     = [512, 680] * 4
 
 POS_CHECK_1   = [770, 200] * 4
 POS_CHECK_2   = [670, 300] * 4
